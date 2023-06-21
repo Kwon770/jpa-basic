84fe691ae9c5c0ebfffd578133a180f856fa4bc9
Kwon770 <sckwon770@gmail.com>
2023-06-22 01:28:53 +0900

    section3: PersistenceContext

--- src/main/java/jpabasic/JpaMain.java
+++ src/main/java/jpabasic/JpaMain.java
@@ -29,15 +29,40 @@ public class JpaMain {
 
 //            findMember.setName("helloJPA");
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-            for (Member m : result) {
-                System.out.println("m.getName() = " + m.getName());
-            }
+
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//            for (Member m : result) {
+//                System.out.println("m.getName() = " + m.getName());
+//            }
 
 
+//            // 비영속
+//            Member member = new Member();
+//            member.setId(5L);
+//            member.setName("helloB");
+//
+//            // 영속
+//            em.persist(member);
+//
+//            Member findMember = em.find(Member.class, 5L);
+//            // SELECT 쿼리 안나감
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            // SELECT 쿼리 안나감
+//            Member findMember2 = em.find(Member.class, 5L);
+//            System.out.println("same instance = " + (findMember == findMember2));
+
+
+            // Dirty checking
+            Member findMember = em.find(Member.class, 5L);
+            findMember.setName("ZZZZZ");
+            // em.persist(findMember) => 부자연스러움. 자바 리스트에서 객체를 불러오고 값을 수정하면 다시 리스트에 넣는가?
+
+            em.flush();
+
+            System.out.println("========= commit");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
